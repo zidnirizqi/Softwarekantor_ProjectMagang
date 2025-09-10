77ff4eee4f0da19fcd886bb1b32c28c99f9256db
Zidni Rizqi <117433786+zidnirizqi@users.noreply.github.com>
2025-09-10 16:18:43 +0700

    update rabu 10 september 2025

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -6,7 +6,7 @@
 <form action="{{ route('admin.user.update', $user->id) }}" method="POST" class="w-50">
     @csrf @method('PUT')
     <div class="mb-3">
-        <label for="name" class="form-label">Nama</label>
+        <label for="name" class="form-label">Name</label>
         <input type="text" name="name" class="form-control" value="{{ $user->name }}" required>
     </div>
     <div class="mb-3">
@@ -16,7 +16,7 @@
     <div class="mb-3">
         <label for="password" class="form-label">Password (opsional)</label>
         <input type="password" name="password" class="form-control">
-        <small class="text-muted">Kosongkan jika tidak ingin mengganti password</small>
+        <small class="text-muted">Leave blank if you don't want to change the password</small>
     </div>
     <button type="submit" class="btn btn-success">💾 Update</button>
     <a href="{{ route('admin.user.index') }}" class="btn btn-secondary">↩️ Back</a>
